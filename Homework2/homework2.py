72703be091a50993401ee22e221cfcfb24254290
Rusu Cristian 1E3 <39808975+Plutonium8T8@users.noreply.github.com>
2023-03-06 19:08:11 +0200

    push

--- Homework2/homework2.py
+++ Homework2/homework2.py
@@ -3,6 +3,8 @@ import numpy as np
 from numpy import linalg
 import math
 import copy
+import tkinter as tk
+from tkinter import ttk
 
 def StrToMatrix(matrix_str):
     matrix = []
@@ -39,20 +41,55 @@ def Bonus(A, L, D, Lt):
     delta = 0.0
     for index1 in range(len(matrix)):
         for index2 in range(len(matrix[0])):
-            print(np.abs(matrix[index1][index2]))
-            print(np.abs(A[index1][index2]))
             delta += np.abs( np.abs(matrix[index1][index2]) -  np.abs(A[index1][index2]))
 
     return "{:.16f}".format(delta)
 
-def Ecuation(A, D, B):
-    pass
+def Ecuation(L, D, Lt, B):
+    Z = []
+    Y = []
+    X = []
+
+    auxiliar = 0
+
+    for index1 in range(0, len(B) - 1):
+        sum = 0
+        for index2 in range(0, index1):
+            sum = L[index1][index2] * Z[index2]
+
+        Z.append(B[index1] - sum)
+
+        auxiliar = index1
+
+    for index in range(auxiliar + 1, len(B)):
+        Z.append(0)
+
+    auxiliar = 0
+
+    for index in range(0, len(D) - 1):
+            Y.append(Z[index] / D[index])
+            auxiliar = index
+
+    for index in range(auxiliar + 1, len(B)):
+        Y.append(0)
+
+    for index1 in range(len(Y) - 1, -1, -1):
+        sum = 0
+        for index2 in range(len(Y) - 1, index1, -1):
+            sum = Lt[index1][index2] * X[len(Y) - index2 - 1]
+
+        X.append(Y[index1] - sum)
+
+    X = X[::-1]
+
+    return Z, Y, X
+
 
 file = open("matrix1.txt", "r")
 matrix_str = file.read()
-matrix1 = StrToMatrix(matrix_str)
+matrix = StrToMatrix(matrix_str)
 
-L, auxD = CholeskyDecomp(np.array(matrix1))
+L, auxD = CholeskyDecomp(np.array(matrix))
 D = []
 for index in range(len(auxD)):
     D.append(auxD[index][index])
@@ -60,9 +97,80 @@ Lt = np.transpose(L)
 T = np.matmul(L, auxD)
 U = Lt
 
-print(L)
-print(D)
-print(Lt)
-print(T)
-print(U)
-print(Bonus(matrix1, L, auxD, Lt))
+file = open("B.txt", "r")
+matrix_str = file.read()
+auxB = StrToMatrix(matrix_str)
+
+B = []
+
+for index in range(len(auxB)):
+    B.append(auxB[index][0])
+
+DStr = ''
+for i in range(len(D)):
+    DStr += str(D[i]) + '\n'
+
+EcuationStr = ''
+for i in range(len(Ecuation(L, D, Lt, B))):
+    if i == 0:
+        EcuationStr += 'Z = ' + str(Ecuation(L, D, Lt, B)[i]) + '\n'
+    elif i == 1:
+        EcuationStr += 'Y = ' + str(Ecuation(L, D, Lt, B)[i]) + '\n'
+    else:
+        EcuationStr += 'X = ' + str(Ecuation(L, D, Lt, B)[i]) + '\n'
+
+def function1():
+    result_label.config(text="L: \n" + str(L))
+
+def function2():
+    result_label.config(text="D: \n" + str(DStr))
+
+def function3():
+    result_label.config( text="Lt: \n" + str(Lt))
+
+def function4():
+    result_label.config( text="T: \n" + str(T))
+
+def function5():
+    result_label.config( text="U: \n" + str(U))
+
+def function6():
+    result_label.config( text="Epsilon = " +  str(Bonus(matrix, L, auxD, Lt)))
+
+def function7():
+    result_label.config( text=EcuationStr)
+
+root = tk.Tk()
+
+style = ttk.Style()
+style.configure("TButton", font=("TkDefaultFont", 12), padding=6)
+
+button1 = tk.Button(root, text="L ", command=function1)
+button1.pack(padx=20, pady=10)
+
+button2 = tk.Button(root, text="D", command=function2)
+button2.pack(padx=20, pady=10)
+
+button3 = tk.Button(root, text="Lt", command=function3)
+button3.pack(padx=20, pady=10)
+
+button4 = tk.Button(root, text="T", command=function4)
+button4.pack(padx=20, pady=10)
+
+button5 = tk.Button(root, text="U", command=function5)
+button5.pack(padx=20, pady=10)
+
+button6 = tk.Button(root, text="Bonus", command=function6)
+button6.pack(padx=20, pady=10)
+
+button7 = tk.Button(root, text="Ecuation(L, D, Lt, B)", command=function7)
+button7.pack(padx=20, pady=10)
+
+
+separator = ttk.Separator(root, orient="horizontal")
+separator.pack(fill="x", padx=20, pady=10)
+
+result_label = ttk.Label(root, font=("TkDefaultFont", 12))
+result_label.pack(padx=20, pady=10)
+
+root.mainloop()
