491cdd36875b0e73616fde36137a5b0d3cbf488c
Rusu Cristian 1E3 <39808975+Plutonium8T8@users.noreply.github.com>
2023-04-27 11:10:24 +0300

    TileMap

--- Homework4/homework4.py
+++ Homework4/homework4.py
@@ -1,131 +1,213 @@
-file = open("a_1.txt", "r")
+import tkinter as tk
+from tkinter import ttk
+
+file = open("a_3.txt", "r")
 
 file_read = file.read()
 
-index = 0
+bfile = open("b_3.txt", "r")
 
-while file_read[index] != '\n':
-    index = index + 1
+bfile_read = bfile.read()
 
-n = int(file_read[0:index])
+a = {}
 
-def diagonal_sum(file_read):
-    diagonal = []
+b = []
+
+aii = []
+
+
+def initializeB(b, bfile_read):
+    auxiliar = 0
+
+    for y in range(0, len(bfile_read)):
+        if bfile_read[y] == '\n':
+            b.append(float(bfile_read[int(auxiliar):y]))
+            auxiliar = y + 1
+
+
+def initializeA(a, file_read):
     auxiliar = 0
     number = 0
-    prev = -1
     i = 0
     j = 0
     count = 0
 
-    nonZeroDiagonal = True
-
-    for y in range(index, len(file_read)):
+    for y in range(0, len(file_read)):
         if file_read[y] == ',':
             count = count + 1
             if count == 1:
-                number = file_read[int(auxiliar):y]
+                number = float(file_read[int(auxiliar):y])
                 auxiliar = y + 1
             if count == 2:
-                i = file_read[int(auxiliar):y]
+                i = int(file_read[int(auxiliar):y])
                 auxiliar = y + 1
         if file_read[y] == '\n':
             count = 0
-            j = file_read[int(auxiliar):y]
+            j = int(file_read[int(auxiliar):y])
             auxiliar = y + 1
 
-            if (i == j):
-                print(i)
-                if (int(i) == prev + 1):
-                    prev = prev + 1
-                else:
-                    break
-    if prev == n - 1: return True
-    else: return False
+            if i not in a.keys():
+                a.update({i: [(j, number)]})
+            else:
+                a[i].append((j, number))
 
-def GaussSneidel(file_read):  
+
+def diagonal_sum(a, aii):
+    previous_key = -1
+
+    for index_dict in a.keys():
+        for index_array in a[index_dict]:
+            if index_dict == index_array[0]:
+                if index_dict != previous_key + 1:
+                    return False
+                aii.append(index_array[1])
+                previous_key = previous_key + 1
+    return True
+
+
+def GaussSneidel(a, b, aii):
     xk = []
+
+    for index in range(len(a.keys())):
+        xk.append(0.0)
+
     for k in range(10000):
-        if k == 0:
-            xk.append(0)
-        for ix in range(n):
+        convergence_factor = 0.0
+
+        norma_partial = 0
+
+        for index_i in a.keys():
+            x_prec = xk[index_i]
             sigma = 0
-            for y in range(index, len(file_read)):
-                if file_read[y] == ',':
-                    count = count + 1
-                    if count == 1:
-                        number = file_read[int(auxiliar):y]
-                        auxiliar = y + 1
-                    if count == 2:
-                        i = file_read[int(auxiliar):y]
-                        auxiliar = y + 1
-                if file_read[y] == '\n':
-                    count = 0
-                    j = file_read[int(auxiliar):y]
-                    auxiliar = y + 1
 
-                    if j < ix - 1:
-                        sigma = sigma + number * xk[k - 1]
-        # xk.append()
+            xk[index_i] = (float(b[index_i]) - sigma) / aii[index_i]
 
-def matrixSum():
-    file1 = open("a.txt", 'r')
-    file2 = open("b.txt", 'r')
+            for index_j in a[index_i]:
+                if index_j[0] < index_i:
+                    sigma += float(index_j[1]) * xk[index_j[0]]
+                else:
+                    sigma += float(index_j[1] * x_prec) 
 
-    file3 = open("auxiliar.txt", 'w')
+            convergence_factor = convergence_factor + abs(xk[index_i] - ((b[index_i] - sigma) / aii[index_i]))
 
-    file_read1 = file1.read()
-    file_read2 = file2.read()
+            norma_partial += (xk[index_i] - x_prec) ** 2
+        
+        norma_partial = norma_partial ** 1/2
 
-    auxiliar = 0
-    number = 0
-    i = 0
-    j = 0
-    count = 0
+            
 
-    for y in range(index, len(file_read1)):
-        if file_read1[y] == ',':
-            count = count + 1
-            if count == 1:
-                number = file_read1[int(auxiliar):y]
-                auxiliar = y + 1
-            if count == 2:
-                i = file_read1[int(auxiliar):y]
-                auxiliar = y + 1
-        if file_read1[y] == '\n':
-            count = 0
-            j = file_read1[int(auxiliar):y]
-            auxiliar = y + 1
+        if norma_partial < 10e-8:
+            print(norma_partial)
 
-            auxiliar1 = 0
-            number1 = 0
-            prev1 = -1
-            i1 = 0
-            j1 = 0
-            count1 = 0
+            file = open("result.txt", "w+")
+            for i in range(len(xk)):
+                file.write(str(xk[i]) + '\n')
+            file.close()
 
-            for y1 in range(index, len(file_read2)):
-                if file_read2[y1] == ',':
-                    count1 = count1 + 1
-                    if count1 == 1:
-                        number1 = file_read2[int(auxiliar1):y1]
-                        auxiliar1 = y + 1
-                    if count == 2:
-                        i1 = file_read2[int(auxiliar1):y1]
-                        auxiliar1 = y1 + 1
-                if file_read2[y] == '\n':
-                    count1 = 0
-                    j1 = file_read2[int(auxiliar1):y1]
-                    auxiliar1 = y1 + 1
+            normalization = []
 
-                    print(i, i1)
+            for i in a.keys():
+                normalization.append(0.0)
 
-                    if i == i1:
-                        if j == j1:
-                            sum = float(number) + float(number1)
-                            print(str(sum) + ', ' + i + ', ' + j + '\n')
-  
-#matrixSum()
+            for i in a.keys():
+                for item in a[i]:
+                    normalization[item[0]] = abs(item[1] * xk[item[0]] - b[item[0]])
+        
+            break
 
-# if diagonal_sum(file_read):
-#     GaussSneidel()
+        # if convergence_factor < 10e-8:
+        #     file = open("result.txt", "w+")
+        #     for i in range(len(xk)):
+        #         file.write(str(xk[i]) + '\n')
+        #     file.close()
+        #     break
+
+    return "Done"
+
+
+def matrixSum(file1, file2):
+    file1 = open("a.txt", 'r').read()
+    file2 = open("b.txt", 'r').read()
+
+    a = {}
+
+    b = {}
+
+    b1 = {}
+
+    initializeA(a, file1)
+
+    initializeA(b, file2)
+
+    for index_i in a.keys():
+        if index_i in b.keys():
+            a[index_i] += b[index_i]
+            for item in b[index_i]:
+                b[index_i].remove(item)
+
+            auxiliar_list = []
+
+            for item in a[index_i]:
+                if item not in auxiliar_list:
+                    auxiliar_list.append(item[0])
+
+            for j in auxiliar_list:
+                sum = 0
+
+                for item in a[index_i]:
+                    if item[0] == j:
+                        sum += item[1]
+                        a[index_i].remove(item) 
+                if sum != 0:
+                    a[index_i].append((j, sum))
+
+    for index_i in b.keys():
+        if index_i not in a.keys():
+            a.update((index_i, b[index_i]))
+            for item in b[index_i]:
+                b[index_i].remove(item)
+
+    file = open("bonus_result.txt", "w+")
+    for i in a.keys():
+        for j in a[i]:
+            file.write(str(i) + ', ' + str(j[0]) + ', ' + str(j[1]) + '\n')
+    file.close()
+    return "Done"
+#if diagonal_sum(a, aii):
+#    matrixSum("a.txt", "b.txt")
+
+initializeA(a, file_read)
+
+initializeB(b, bfile_read)
+
+# if diagonal_sum(a, aii):
+#     print(GaussSneidel(a, b, aii))
+
+def function1():
+    if diagonal_sum(a, aii):
+        result_label.config(text=str(GaussSneidel(a, b, aii)))
+        return 
+
+def function2():
+    if diagonal_sum(a, aii):
+        result_label.config(text = str(matrixSum("a.txt", "b.txt")))
+
+
+root = tk.Tk()
+
+style = ttk.Style()
+style.configure("TButton", font=("TkDefaultFont", 12), padding=6)
+
+button1 = tk.Button(root, text="GaussSneidel", command=function1)
+button1.pack(padx=20, pady=10)
+
+button2 = tk.Button(root, text="Bonus", command=function2)
+button2.pack(padx=20, pady=10)
+
+separator = ttk.Separator(root, orient="horizontal")
+separator.pack(fill="x", padx=20, pady=10)
+
+result_label = ttk.Label(root, font=("TkDefaultFont", 12))
+result_label.pack(padx=20, pady=10)
+
+root.mainloop()
